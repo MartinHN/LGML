9edbaa56fa69af2401be8e791ed295c38e6fc083
Martin Hermant <martin.hermant@gmail.com>
2016-11-06 00:50:22 +0100

    wip

--- src/LGML/Source/GitSha.h
+++ src/LGML/Source/GitSha.h
@@ -1,4 +1,4 @@
 #pragma once
-#define GIT_SHA  "4d5aaf5ff42379ba6dfb4df24b5911a88d85c814"
-#define GIT_SHA_SHORT  "4d5aaf5"
-#define COMMIT_DATE  "06/11/16 (00:48)"
+#define GIT_SHA  "a0d1b30df46eaf2b885c737e65598883b6dd31fe"
+#define GIT_SHA_SHORT  "a0d1b30"
+#define COMMIT_DATE  "06/11/16 (00:50)"
