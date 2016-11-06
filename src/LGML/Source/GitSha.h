1b1a7d8a5848a2c57986a57ca1dd390d34421490
Martin Hermant <martin.hermant@gmail.com>
2016-11-06 05:26:59 +0100

    yoyo

--- src/LGML/Source/GitSha.h
+++ src/LGML/Source/GitSha.h
@@ -1,4 +1,4 @@
 #pragma once
-#define GIT_SHA  "7b7bd85f2dfaa4609d297b3a7b41739db48058bf"
-#define GIT_SHA_SHORT  "7b7bd85"
-#define COMMIT_DATE  "06/11/16 (05:12)"
+#define GIT_SHA  "0e9226dfb9724b6c6e072d3dac70973013a81d84"
+#define GIT_SHA_SHORT  "0e9226d"
+#define COMMIT_DATE  "06/11/16 (05:26)"
