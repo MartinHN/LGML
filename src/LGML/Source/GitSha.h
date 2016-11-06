7b7bd85f2dfaa4609d297b3a7b41739db48058bf
Martin Hermant <martin.hermant@gmail.com>
2016-11-06 03:38:47 +0100

    reoups

--- src/LGML/Source/GitSha.h
+++ src/LGML/Source/GitSha.h
@@ -1,4 +1,4 @@
 #pragma once
-#define GIT_SHA  "0f2df9769f0cec8dc7ad8c970b313432996ecdcf"
-#define GIT_SHA_SHORT  "0f2df97"
-#define COMMIT_DATE  "06/11/16 (03:05)"
+#define GIT_SHA  "1d36e90977d0b47bc98aa7ab4b258f59007693fb"
+#define GIT_SHA_SHORT  "1d36e90"
+#define COMMIT_DATE  "06/11/16 (03:38)"
