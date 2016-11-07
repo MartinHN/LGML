7207d85e8584cbb0cacba96837094ae9a1401b83
Martin Hermant <martin.hermant@gmail.com>
2016-11-07 19:12:03 +0100

    force 64 arch

--- src/LGML/Source/GitSha.h
+++ src/LGML/Source/GitSha.h
@@ -1,4 +1,4 @@
 #pragma once
-#define GIT_SHA  "8a14e9edc718d671392644e82a64779ab4de87f9"
-#define GIT_SHA_SHORT  "8a14e9e"
-#define COMMIT_DATE  "07/11/16 (17:59)"
+#define GIT_SHA  "cd3b40fd7669cbbee057d95813f03af580b0405d"
+#define GIT_SHA_SHORT  "cd3b40f"
+#define COMMIT_DATE  "07/11/16 (19:12)"
