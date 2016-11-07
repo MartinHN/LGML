8a14e9edc718d671392644e82a64779ab4de87f9
Martin Hermant <martin.hermant@gmail.com>
2016-11-07 17:31:06 +0100

    defines colours as  static

--- src/LGML/Source/GitSha.h
+++ src/LGML/Source/GitSha.h
@@ -1,4 +1,4 @@
 #pragma once
-#define GIT_SHA  "5723388f56469d6003c56aaeebff5835e94ade81"
-#define GIT_SHA_SHORT  "5723388"
-#define COMMIT_DATE  "07/11/16 (15:46)"
+#define GIT_SHA  "1a91fd967d4b54217bece1cd718d39daaf885d35"
+#define GIT_SHA_SHORT  "1a91fd9"
+#define COMMIT_DATE  "07/11/16 (17:31)"
