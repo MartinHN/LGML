0ffd94acdac868335cfc188458dc31bc8088f7c4
Martin Hermant <martin.hermant@gmail.com>
2016-11-07 15:29:14 +0100

    fix home dir

--- src/LGML/Source/GitSha.h
+++ src/LGML/Source/GitSha.h
@@ -1,4 +1,4 @@
 #pragma once
-#define GIT_SHA  "1f0c3a7029ade44d0eedf487d5223447d0ff000b"
-#define GIT_SHA_SHORT  "1f0c3a7"
-#define COMMIT_DATE  "07/11/16 (15:27)"
+#define GIT_SHA  "2e1191e1ecd66b38b6a69bf9059095ff7f26cb07"
+#define GIT_SHA_SHORT  "2e1191e"
+#define COMMIT_DATE  "07/11/16 (15:29)"
