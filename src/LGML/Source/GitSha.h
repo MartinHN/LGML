d03ce6658ac76b57037cf993dbfc7036da3a4749
Martin Hermant <martin.hermant@gmail.com>
2016-11-07 19:38:05 +0100

    seems working with arch 64

--- src/LGML/Source/GitSha.h
+++ src/LGML/Source/GitSha.h
@@ -1,4 +1,4 @@
 #pragma once
-#define GIT_SHA  "cd3b40fd7669cbbee057d95813f03af580b0405d"
-#define GIT_SHA_SHORT  "cd3b40f"
-#define COMMIT_DATE  "07/11/16 (19:12)"
+#define GIT_SHA  "7207d85e8584cbb0cacba96837094ae9a1401b83"
+#define GIT_SHA_SHORT  "7207d85"
+#define COMMIT_DATE  "07/11/16 (19:38)"
